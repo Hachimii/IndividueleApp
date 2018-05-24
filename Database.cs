337421fd199249c55886985b36938d98dcdab041
Hachimii <36228365+Hachimii@users.noreply.github.com>
2018-05-24 10:48:42 +0200

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FTHWebapp
+{
+    public class Database
+    {
+        public SqlConnection Connection;
+        public string UserID;
+        public string Password;
+        public string DB;
+        public string Server;
+
+
+
+        public bool Openconnection()
+        {
+
+            string connectionString = "Server=mssql.fhict.local;Database=dbi383927;User Id=dbi383927;Password=Huiswerk1-";
+
+            Connection = new SqlConnection(connectionString);
+
+            try
+            {
+                Connection.Open();
+                return true;
+
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                Connection.Close();
+                return true;
+            }
+            catch (SqlException)
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FTHWebapp.Models;
+using FTHWebapp.Models.DomainClasses;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using FTHWebapp.SqlContext;
+using FTHWebapp.Interfaces;
+
+namespace FTHWebapp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddIdentity<Volunteer, Role>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
